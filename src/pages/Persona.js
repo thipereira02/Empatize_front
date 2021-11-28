3bc0e4f9a0eee6be654861d0e513f105f871a2e7
thipereira02 <thiago.spereira02@gmail.com>
2021-11-28 10:32:44 -0300

    Adding button to go to the next page

--- src/pages/Persona.js
+++ src/pages/Persona.js
@@ -21,8 +21,11 @@ export default function Persona() {
 				:
 				<Text>
 					<img src="https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg?size=626&ext=jpg" alt="Persona" />
-					<p>Você tem 24 anos e é uma mulher trans.
+					<div>
+						<h1>Luana</h1>
+						<p>Você tem 24 anos e é uma mulher trans.
 Recentemente, após muitas tentativas, você conseguiu um emprego na sua área, de comunicação. Seus pais de expulsaram de casa após você assumir sua identidade. Você tem dormido na casa de amigos e tem medo de utilizar o transporte público, com receio de ser agredida. Às vezes pega carona, mas muitas vezes gasta muito com aplicativos de transporte para chegar ao trabalho. </p>
+					</div>
 				</Text>
 			}
 			{disabled ? 
@@ -68,15 +71,16 @@ const Question = styled.h1`
 
 const Title = styled.h2`
     margin-top: 47px;
-    margin-bottom: 80px;
+    margin-bottom: 40px;
     font-weight: 800;
     font-size: 50px;
     line-height: 60px;
+
 `;
 
 const Action = styled(FaPlay)`
     position: absolute;
-    top: 460px;
+    top: 420px;
     right: 645px;
     font-size: 50px;
     cursor: pointer;
@@ -88,11 +92,22 @@ const Text = styled.div`
     align-items: center;
     flex-wrap: nowrap;
 
+    div{
+        margin-left: 100px;
+    }
+
+    h1{
+        font-weight: 800;
+        font-size: 50px;
+        line-height: 60px;
+        margin-bottom: 15px;
+        color: #F8ADFF;
+    }
+
     p{
         font-weight: 800;
         font-size: 25px;
         line-height: 30px;
-        margin-left: 100px;
     }
 `;
 
